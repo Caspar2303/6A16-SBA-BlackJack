5c8792743af80c4a267d91b85b6efa71b4c5fbde
Caspar2303 <himhimwong222@gmail.com>
2026-08-25 22:39:34 +0800

    Update bet validation message

--- app.py
+++ app.py
@@ -359,7 +359,7 @@ def game():
                     <input type="hidden" name="action" value="start_round">
                     <div style="margin: 20px 0;">
                         <label style="font-size: 18px;">Enter Bet Amount ($):</label><br>
-                        <input type="number" name="bet" value="100" min="1" max="{session['bank']}" class="bet-input" required>
+                        <input type="number" name="bet" value="100" min="1" max="{session['bank']}" class="bet-input" required oninvalid="this.setCustomValidity('Value must be greater than or equal to 1.')" oninput="this.setCustomValidity('')">
                     </div>
                     <button type="submit" class="btn-deal">🃏 Deal</button>
                 </form>
